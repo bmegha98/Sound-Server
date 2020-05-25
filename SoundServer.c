49ea398234952734863d8035c040f6652f16fcb4
Megha Bisht <bishtmegha98@gmail.com>
2020-05-25 12:41:04 +0530

    Update SoundServer.c

--- SoundServer.c
+++ SoundServer.c
@@ -117,8 +117,8 @@ int main(int argc , char** argv)
    listenfd = socket(AF_INET, SOCK_STREAM, 0);
    bzero(&servaddr, sizeof(servaddr));
    servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);   		//Foreign IP address
-   servaddr.sin_port = htons(SERV_PORT);             		//Foreign Port Number
+   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);   		//Local IP address
+   servaddr.sin_port = htons(SERV_PORT);             		//Local Port Number
    printf("********TCP Client-Server Application*********\n");
    printf("******************TCP SERVER******************\n");
    
